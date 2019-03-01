565681b9555c8ff1d866987c8f2a315780bbc757
prasad-muddala <48097826+prasad-muddala@users.noreply.github.com>
2019-03-01 13:09:44 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,19 +1,19 @@
 method-1
----------------------------
+
 from operator import mul
-from functools import reduce
+from functools import reduce#to know about reduce function go to readme and read about reduce and map functions
 n=int(input())
 list=[]
 for i in range(1,n+1):
 	list.append(i)
 	k=reduce(mul,list[:])
 print(k)
----------------------------
+
 """in the above program we imported "mul" which basically multiplies two values instead of importing it we can simply define a function
 for "mul" as shown in the below program"""
 
 method-2
-------------------------------------------------
+
 #declare a function for finding the product
 def fact(x,y):
 	return x*y
@@ -21,4 +21,4 @@ n=int(input("enter a value:"))
 f=reduce(fact,range(1,n+1))
 
 print(f)
-------------------------------------------------
+
